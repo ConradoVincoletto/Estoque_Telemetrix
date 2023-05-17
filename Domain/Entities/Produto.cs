d8bd5b634e7671d5fca92c70dcdec4951622f8b9
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-17 20:08:48 -0300

    feat: annotations entidade produto

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+    [Table("TB_PRODUTO")]
     public class Produto
     {
+        [Key]
+        [Column("PRODUTO_ID")]
+        [Display(Name ="CÓDIGO_PRODUTO")]
         public int Id { get; set; }
+
+        [Column("PRODUTO_NOME")]       
         public string Nome { get; set; }
+
+        [Column("PRODUTO_CATEGORA")]        
         public Categoria categoria { get; set; }
+
+        [Column("PRODUTO_QUANTIDADE")]        
         public decimal QuantidadeDisponivel { get; set; }
+
+        [Column("PRODUTO_DATA")]
         public DateTime DataCriacao { get; set; }
+
+        [Column("PRODUTO_ATIVO")]
         public bool Ativo { get; set; }
     }
 }
