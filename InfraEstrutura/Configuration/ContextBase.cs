9fefff1b5b12dcb100564facac8e26c87f7847c7
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-17 20:43:57 -0300

    feat: Classe ContextBase

---
+++ InfraEstrutura/Configuration/ContextBase.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfraEstrutura.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> categorias { get; set; }
+        public DbSet<Produto> produtos { get; set; }
+
+    }
+}
