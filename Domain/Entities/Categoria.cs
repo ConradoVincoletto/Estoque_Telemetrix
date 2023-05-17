27c4735ac75b8a738b945c0d366eb973d342cc1f
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-05-17 19:55:04 -0300

    feat: categoria produto

---
+++ Domain/Entities/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool ControlaEstoque { get; set; }
+        public int MaximoItens { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
